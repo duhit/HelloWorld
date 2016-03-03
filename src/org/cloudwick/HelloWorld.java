6d7472c673b8b6c045bdcbcfa4ec73fb869d017e
Duhit Choudhary <Duhit Choudhary>
2016-03-03 11:58:58 +0000

    added first file

---
+++ src/org/cloudwick/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.cloudwick;
+
+public class HelloWorld {
+	public void printHelloWorld(){
+		System.out.println("Hello World!");
+	}
+	public static void main(String[] args) {
+		HelloWorld object = new HelloWorld();
+		object.printHelloWorld();
+	}
+}
